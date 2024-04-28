6b8b4dc5a2ccdddb9f65cb604c55c70fb5be71ec
AbdulQohar1 <adebayoabdulqohar1@gmail.com>
2024-04-28 22:07:07 +0100

    Server side setup update

--- app.js
+++ app.js
@@ -11,22 +11,28 @@ const io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-let socketsConnected = new Set()
+let socketsConnected = new Set();
 
 io. on('connection', onConnected);
 
 function onConnected(socket) {
   console.log(socket.id);
-  socketsConnected.add(socket.id);
+  socketsConnected.add(socket.id)
 
-  io.emit('clients-total', socketsConnected.size);
+  io.emit('clients-total', socketsConnected.size)
 
   socket.on('disconnect' , () => {
     console.log('Socket disconnected' , socket.id);
     socketsConnected.delete(socket.id);
 
     io.emit('clients-total', socketsConnected.size);
+  });
+
+  socket.on('message' , (data) => {
+    console.log(data);
+    socket.broadcast.emit('chat-message' , data)
   })
+
 
 }
 
